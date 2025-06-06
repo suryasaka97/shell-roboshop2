b7cbe27da50664bd1c6b70f55e55cc88e50e01ec
suryapradeepsaka <suryapradeepsaka@gmail.com>
2025-06-06 16:58:22 +0530

    creating all ec2 instances

---
+++ Basic_roboshop.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-01659ab712a1c0efe"
+ZONE_ID="Z0223797DGGO4EOPINIX"
+DOMAIN_NAME="anantya.space"
+Instance_type="t2.micro"
+
+
+##Basic script to create aws instance###
+aws ec2 run-instances \
+  --image-id $AMI_ID \
+  --instance-type $Instance_type \
+  --security-group-ids $SG_ID \
+  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=frontend}]' \
+  --output json   ###Tags are used to provide names### 
+
+##Command used to see the particular Instance##
+aws ec2 describe-instances \
+  --instance-ids i-0cd1d9c1133444f49
+
+##Command to get Public IP address using query
+aws ec2 describe-instances --instance-ids i-0cd1d9c1133444f49 \
+  --query "Reservations[0].Instances[0].PublicIpAddress" \
+  --output text
+
+##Command to get instance_ID
+INSTANCE_ID=$(aws ec2 run-instances \
+  --image-id ami-0abcdef1234567890 \  # Replace with your AMI ID
+  --instance-type t2.micro \
+  --key-name my-keypair \             # Replace with your key name
+  --security-group-ids sg-12345678 \  # Replace with your SG ID
+  --subnet-id subnet-12345678 \       # Replace with your subnet ID
+  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=myServer1}]' \
+  --query 'Instances[0].InstanceId' \
+  --output text)
+
+##command to get Ip_address
+INSTANCE_IP=$(aws ec2 describe-instances \
+  --instance-ids $INSTANCE_ID \  ##$Instance_ID is variable used fom above command
+  --query 'Reservations[0].Instances[0].PublicIpAddress' \
+  --output text)
+
+echo "Public IP Address: $INSTANCE_IP"
--- roboshop.sh
+++ roboshop.sh
@@ -1,14 +1,37 @@
-#! /bin/bash
+#!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-01659ab712a1c0efe"
 ZONE_ID="Z0223797DGGO4EOPINIX"
 DOMAIN_NAME="anantya.space"
-Instance_type="t2.micro"
+Type="t2.micro"
 
-aws ec2 run-instances \
+##To create multiple instances at one go
+Instance=("catlouge" "users" "frontend" "mysql" "mongodb" "rabitnq" "redis" "dispatch" "payment" "shipping" "cart")
+
+
+for instance in ${Instance[@]}
+do
+    Instance_ID=$(aws ec2 run-instances \
   --image-id $AMI_ID \
-  --instance-type $Instance_type \
+  --instance-type $Type \
   --security-group-ids $SG_ID \
-  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=frontend}]' \
-  --output json
+  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' \
+  ----query 'Instances[0].InstanceId' \
+  --output text)
+   
+    if [ $instance != frontend ]
+    then 
+         ip=$(aws ec2 describe-instances --instance-ids $Instance_ID \
+        --query "Reservations[0].Instances[0].PrivateIpAddress" \
+        --output text)
+    else
+        ip=$(aws ec2 describe-instances --instance-ids $Instance_ID \
+        --query "Reservations[0].Instances[0].PublicIpAddress" \
+        --output text)
+    fi
+
+    echo "$instance : $ip" 
+done
+ 
+
