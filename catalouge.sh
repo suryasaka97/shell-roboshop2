49319421f972473e26a4d7db9989f9fad59d1d4c
suryapradeepsaka <suryapradeepsaka@gmail.com>
2025-06-10 12:14:48 +0530

    Mongodb and catalouge

--- catalouge.sh
+++ catalouge.sh
@@ -14,10 +14,10 @@ file_name=$(echo $0 | cut -d "." -f1)
 file_path="$Log_Folder/$file_name.log"
 script_path=$(PWD)
 
+mkdir -p $Log_Folder
 
 echo "you are running this script at $(date)" | tee -a $file_path
-
-mkdir -p $Log_Folder 
+ 
 
 if [ $user -ne 0 ]
 then
@@ -39,52 +39,52 @@ fi
 }
 
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>$file_path
 validate $? "Disable default nodejs module"
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y  &>>$file_path
 validate $? "Enable nodejs:20 Module"
 
-dnf install nodejs -y
+dnf install nodejs -y  &>>$file_path
 validate $? "Installing Nodejs"
 
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>$file_path
 validate $? "Creating Roboshop user"
 
 mkdir /app 
 validate $? "/app folder is created"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$file_path
 validate $? "Downloading catalogue.zip to tmp folder"
 
 cd /app 
 validate $? "moving to /App"
 
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip  &>>$file_path
 validate $? "Unzipping catalogue zip file in /app folder"
 
 
 cd /app 
-npm install 
+npm install &>>$file_path
 validate $? "npm installation"
 
-cp $script_path/catalogue.service /etc/systemd/system/catalogue.service
+cp $script_path/catalogue.service /etc/systemd/system/catalogue.service  &>>$file_path
 validate $? "copying daemon path"
 
-systemctl daemon-reload
+systemctl daemon-reload  &>>$file_path
 validate $? "daemon-reload"
 
-systemctl enable catalogue 
+systemctl enable catalogue  &>>$file_path
 systemctl start catalogue
 validate $? "start catalogue"
 
-cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo
+cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo  &>>$file_path
 validate $? "copying mongo repo"
 
-dnf install mongodb-mongosh -y
+dnf install mongodb-mongosh -y  &>>$file_path
 validate $? "installing mongodb client"
 
-mongosh --host mongodb.anantya.space </app/db/master-data.js
+mongosh --host mongodb.anantya.space </app/db/master-data.js  &>>$file_path
 validate $? "Loading data into mongodb"
 
 
