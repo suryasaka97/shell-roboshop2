5986ac51f44690c62e5619f9f51ac0779dd98eab
suryapradeepsaka <suryapradeepsaka@gmail.com>
2025-06-17 21:52:16 +0530

    shipping

--- mysql.sh
+++ mysql.sh
@@ -49,7 +49,7 @@ systemctl enable mysqld &>> $file_path
 systemctl start mysqld  &>> $file_path
 validate $? "starting mysql"
 
-read -sp "please provide roboshop password" MYSQL_PASSWORD &>>$file_path
+read -sp "please provide roboshop password" MYSQL_PASSWORD
 
 mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD 
 validate $? "setting root password"
